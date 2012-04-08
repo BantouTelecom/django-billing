30d45e40238c06a69ab1c7eba2a535fe1c25e221
Jérôme Petazzoni <jp@enix.org>
2012-04-08 13:05:55 -0700

    use old-style namespace package do work around django bug

--- setup.py
+++ setup.py
@@ -14,7 +14,10 @@ setup(
         'billing_management.management',
         'billing_management.management.commands',
     ],
-    namespace_packages=['billing', 'billing.processor'],
+    # django.utils.translation.trans_real tries to access <app>.__file__,
+    # which does not exist when namespace packages are declared that way.
+    # Let's use the "old" way until django.utils.translation is fixed.
+    #namespace_packages=['billing', 'billing.processor'],
     license='AGPL',
     long_description=open('README').read(),
     install_requires=[
